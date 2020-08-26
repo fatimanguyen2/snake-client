b31ca82fa33950a4ca9d9a49d0b5232a7fc98c6f
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-26 16:28:02 +0000

    make client file and name my snake

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: FN')
+  });
+  conn.setEncoding('utf8'); 
+  
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+}
+
+module.exports = connect;
